f018f53e9f45eaf1e3efee3bcd039f0400251cb7
JS Kim <iai6203@gmail.com>
2023-09-26 14:23:38 +0900

    feat: CRA - TailwindCSS, Typescript 템플릿 추가

---
+++ cra-tailwindcss-typescript/prettier.config.cjs
@@ -0,0 +1,30 @@
+/**
+ * @type {
+ *   import("prettier").Config &
+ *   import("@trivago/prettier-plugin-sort-imports").PrettierConfig
+ * }
+ */
+const config = {
+  tabWidth: 2,
+  trailingComma: "all",
+  semi: false,
+  singleQuote: true,
+
+  "importOrder": [
+    "^@app/(.*)$",
+    "^@pages/(.*)$",
+    "^@components/(.*)$",
+    "^@hooks/(.*)$",
+    "^@styles/(.*)$",
+    "^[./]"
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  plugins: [
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+}
+
+module.exports = config
