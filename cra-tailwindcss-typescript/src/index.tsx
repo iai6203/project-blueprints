5e3ddcc6c5a00691bf88f925993b1e80dfac59a9
JS Kim <iai6203@gmail.com>
2023-09-27 14:59:29 +0900

    fix: 올바르지 않은 css import 경로 수정

--- cra-tailwindcss-typescript/src/index.tsx
+++ cra-tailwindcss-typescript/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from '@app/App'
 
-import './index.css'
+import '@styles/tailwind.css'
 
 const rootElement = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(rootElement)
